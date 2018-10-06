34899c46a1fbfb20092af9813778a4ee2b16578c
Vanessa Martin <vanessa.s.martin96@gmail.com>
2018-10-05 21:47:01 -0700

    first commit

---
+++ public/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'public';
+  constructor (private _data: DataService){}
+}
